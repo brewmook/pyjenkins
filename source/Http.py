f06fdfebcd63233a037bbc11966e69c4985896d6
Dave E <dave.git.2012@coolhandmook.com>
2011-11-09 17:38:47 +0000

    First stab at Http implementation. Not tested.

--- source/Http.py
+++ source/Http.py
@@ -1,3 +1,5 @@
+import httplib, urllib
+
 # Response constants
 OK = 200
 NOT_FOUND = 404
@@ -14,3 +16,43 @@ class IHttp:
         Return tuple of (contents, returnCode).
         '''
         pass
+
+class Http(IHttp):
+    '''
+    Method implementations loosely based on examples at
+    http://docs.python.org/release/2.6.7/library/httplib.html
+    '''
+
+    def __init__(self, host):
+        self.host= host
+
+    def getUrl(self, url):
+
+        conn = httplib.HTTPConnection(self.host)
+        conn.request("GET", "/"+url)
+        response= conn.getresponse()
+
+        status= response.status
+        content= response.read()
+
+        conn.close()
+
+        return (content, status)
+
+    def post(self, url, postData, arguments={}):
+
+        params = urllib.urlencode(arguments)
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "Accept": "text/plain"}
+
+        conn = httplib.HTTPConnection(self.host)
+        conn.request("POST", "/"+url, params, headers)
+        conn.send(postData)
+
+        response= conn.getresponse()
+        status= response.status
+        content= response.read()
+
+        conn.close()
+
+        return (content, status)
