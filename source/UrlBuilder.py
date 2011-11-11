935d5aedb22ee8f065746bd4bc230ddda34ef973
Dave E <dave.git.2012@coolhandmook.com>
2011-11-11 10:46:48 +0000

    Removed protocol argument from UrlBuilder.build(), added UrlBuilderFactory

--- source/UrlBuilder.py
+++ source/UrlBuilder.py
@@ -1,22 +1,33 @@
 import urllib
 
+class IUrlBuilderFactory:
+
+    def create():
+        pass
+
 class IUrlBuilder:
 
-    def build(self, protocol, host, path, arguments={}):
+    def build(self, host, path, arguments={}):
         '''
         Create a well-formed url from the constituent parts.
         '''
+        pass
 
 class UrlBuilder:
 
     def __init__(self):
         pass
 
-    def build(self, protocol, host, path, arguments={}):
+    def build(self, host, path, arguments={}):
 
-        url= '%s://%s/%s' % (protocol, host, urllib.quote(path))
+        url= '%s/%s' % (host, urllib.quote(path))
 
         if arguments:
             url= '?'.join([url, urllib.urlencode(arguments)])
             
         return url
+
+class UrlBuilderFactory(IUrlBuilderFactory):
+
+    def create():
+        return UrlBuilder()
--- tests/UrlBuilderTests.py
+++ tests/UrlBuilderTests.py
@@ -7,9 +7,9 @@ class UrlBuilderTests(TestCase):
     def test_build_NoArguments_ReturnCorrectlyJoinedUrl(self):
 
         builder= UrlBuilder()
-        result= builder.build('http', 'host', 'path')
+        result= builder.build('host', 'path')
 
-        self.assertEqual('http://host/path', result)
+        self.assertEqual('host/path', result)
 
     def test_build_WithArguments_ReturnCorrectlyJoinedUrl(self):
 
@@ -17,13 +17,13 @@ class UrlBuilderTests(TestCase):
                     'xyz':'abc'}
 
         builder= UrlBuilder()
-        result= builder.build('http', 'host', 'path', arguments)
+        result= builder.build('host', 'path', arguments)
 
-        self.assertEqual('http://host/path?blah=whatever&xyz=abc', result)
+        self.assertEqual('host/path?blah=whatever&xyz=abc', result)
 
     def test_build_SpacesInPath_ReturnSpacesEscaped(self):
 
         builder= UrlBuilder()
-        result= builder.build('http', 'host', 'path with spaces')
+        result= builder.build('host', 'path with spaces')
 
-        self.assertEqual('http://host/path%20with%20spaces', result)
+        self.assertEqual('host/path%20with%20spaces', result)
